cb3806b07324da88fa09aa6f6d46712ede24308b
Andreas <a.sutt@khk.ee>
2022-02-09 18:23:18 +0200

    Completed app with Backend

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express')
-const app  = express()
+const app = express()
 
 app.set('view engine', 'pug')
+app.use(express.urlencoded())
 
-app.get('/', function (req, res){
+app.use('/', function (req, res) {
+    let Email = req.body.email
+    let message = null
+    if (req.body.email) {
+        if (Email.includes("@") && Email.includes(".")) {
+            message = "This email is valid"
+        } else {
+            message = "This is not a valid Email"
+        }
+    }
     res.render('index', {
-        title:'Hey',
-        message:'日本の出生率は大幅に低下しています。'
+        title: 'Hey',
+        message
     })
 })
 
-app.listen(4000, function (err){
-    if(err){
+
+
+app.listen(4000, function (err) {
+    if (err) {
         console.log(err)
-    }else{
+    } else {
         console.log('Connection has been established on port 4000')
     }
 })
